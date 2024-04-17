7983607a75791312dbc8efca374719375f1c0d87
Mariya Khan <jiyarehman80@gmail.com>
2024-04-17 13:25:02 -0700

    again submit

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 //Declare a constant answer and assigned
-
-const answers: { sentence: string } = await inquirer.prompt([
-  {
-    name: "sentence",
-    type: "input",
-    message: "Enter your sentence to count the word: ",
-  },
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: ",
+    },
 ]);
 //Declare after initialization execute block of code
-
-const words= answers.sentence.trim().split(" ")
+const words = answers.sentence.trim().split(" ");
 //print the array of words to the console
 console.log(words);
-
-
 //Print the word count of the sentence to the console
 console.log(`Your sentence word count is ${words.length}`);
-
-
